3a6df23ae68753aaaf1ed656b5261681f1aec585
Sanika Joshi <Sanikaj0605@gmail.com>
2026-09-11 16:56:02 +0530

    auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,10 +13,22 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Only registered users can login
+authenticated.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in. Username and password required." });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
