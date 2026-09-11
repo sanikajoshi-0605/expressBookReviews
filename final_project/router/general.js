6ea2c1d2d36be077500078d386af348d2abeea19
Sanika Joshi <Sanikaj0605@gmail.com>
2026-09-11 16:50:25 +0530

    general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,26 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Register a new user
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Unable to register user. Username and password required." });
+  }
+
+  const isValid = (username) => {
+    let userswithsamename = users.filter((user) => user.username === username);
+    return userswithsamename.length > 0;
+  };
+
+  if (!isValid(username)) {
+    users.push({ "username": username, "password": password });
+    return res.status(200).json({ message: "User successfully registered. Now you can login" });
+  } else {
+    return res.status(404).json({ message: "User already exists!" });
+  }
 });
 
 // Get the book list available in the shop
