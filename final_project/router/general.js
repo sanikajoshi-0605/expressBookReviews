cd1a4d2cc1c2126eb3a95b683fad095cba8b3780
Sanika Joshi <Sanikaj0605@gmail.com>
2026-09-11 16:33:25 +0530

    general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,10 +63,14 @@ public_users.get('/title/:title', function (req, res) {
   }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
